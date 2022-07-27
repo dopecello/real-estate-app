9464f74a28f3577e0682739a15c45c07e1bc91fd
dopecello <williamlocke.cello@gmail.com>
2022-07-26 22:29:01 -0400

    got API to finally cooperate

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,38 @@
 // UNIVERSAL SELECTORS
 const searchBtn = document.getElementById("searchBtn")
 
-
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '8a074ef487mshdab54e7691d32ffp1dcf75jsn10bed4368ac3',
+		'X-RapidAPI-Key': 'ba4b449754mshe56b9d6fec584cbp1afc6fjsn27231b1cf8a7',
 		'X-RapidAPI-Host': 'realty-in-us.p.rapidapi.com'
 	}
 };
 
-let stateCode = document.getElementById("stateDropDown").value
-let city = document.getElementById("searchCity").value
-let beds = document.getElementById("minBedsSelect").value
+// let stateCode = 'FL'
+// let city = 'Miami'
+// let beds = '1'
 
 
-	
-document.getElementById("searchBtn").addEventListener("click", getAPIData())
+searchBtn.onclick = function getAPIData(event) {
 
-function getAPIData () {
+	let stateCode = document.getElementById("stateDropDown").value
+	let city = document.getElementById("searchCity").value
+	let beds = document.getElementById("minBedsSelect").value
+
 	fetch(
-		"https://realty-in-us.p.rapidapi.com/properties/list-for-sale?" +
-		'state_code=' + 
-		 stateCode + 
-		 '&city=' + 
-		 city + 
-		 '&limit=50&offset=0&sort=relevance' + 
-		 '&beds_min=' + 
-		 beds
-		 , options)
-			.then(response => response.json())
-			.then(response => console.log(response))
-			.catch(err => console.error(err));
+		'https://realty-in-us.p.rapidapi.com/properties/list-for-rent?' +
+		'state_code=' +
+		stateCode +
+		'&city=' +
+		city +
+		'&limit=50&offset=0&sort=relevance' +
+		'&beds_min=' +
+		beds
+		, options)
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.catch(err => console.error(err));
+		
+	event.preventDefault();
 }
-	
- 
-
-
-
-
-    
-    
