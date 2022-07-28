d904c5bbde4337b93fc270a4b59057e735c216a2
dopecello <williamlocke.cello@gmail.com>
2022-07-27 22:04:16 -0400

    saving point for functionality

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 // UNIVERSAL SELECTORS
 const searchBtn = document.getElementById("searchBtn")
+const cardContainer = document.getElementById("cardContainer")
 
 const options = {
 	method: 'GET',
@@ -21,18 +22,41 @@ searchBtn.onclick = function getAPIData(event) {
 	let beds = document.getElementById("minBedsSelect").value
 
 	fetch(
-		'https://realty-in-us.p.rapidapi.com/properties/list-for-rent?' +
+		'https://realty-in-us.p.rapidapi.com/properties/list-for-sale?' +
 		'state_code=' +
 		stateCode +
 		'&city=' +
 		city +
-		'&limit=50&offset=0&sort=relevance' +
+		'&limit=6&offset=0&sort=relevance' +
 		'&beds_min=' +
 		beds
 		, options)
-		.then(response => response.json())
-		.then(response => console.log(response))
+		.then((res) => res.json())
+		.then((data) => {
+			let propertyListings = data.listings
+			// var propDivEl = [];
+			// var propImgEl = [];
+			for (var i = 0; i < propertyListings.length; i++) {
+				const property = propertyListings[i]
+				console.log(property.address)
+				// propDivEl[i] = document.createElement("div");
+				// propDivEl.classList.add("card");
+				// var propImgEl = document.createElement("img");
+				// propImgEl
+			}
+		})
+
 		.catch(err => console.error(err));
-		
+
 	event.preventDefault();
+
 }
+	// `<div class="card">
+	// 			<img src=${listings.photo} alt="listings-image">
+	// 			<p><span id="price">${listings.price} </span><span id="prop-type">${listings.prop_type}</span></p>
+	// 			<p><span id="bath">${listings.baths} Baths </span>-<span id="bed"> ${listings.beds} Beds</span></p>
+	// 			<p id="address">${listings.address}</p>
+	// 			<br />
+	// 			<a href=${listings.rdc_web_url} id="details">Details</a>
+	// 			</div>`
+	// 		})
